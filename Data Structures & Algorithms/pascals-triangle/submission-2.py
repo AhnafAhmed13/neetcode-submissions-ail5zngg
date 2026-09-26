dc02be49909557fed946a97a9d713f4352725edd
Ahnaf Ahmed <61428434+AhnafAhmed13@users.noreply.github.com>
2026-09-25 20:16:38 -0400

    Add: pascals-triangle - submission-2

---
+++ Data Structures & Algorithms/pascals-triangle/submission-2.py
@@ -0,0 +1,12 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        if numRows == 1: return [[1]]
+        if numRows == 2: return [[1], [1, 1]]
+        res = [[1], [1, 1]]
+        for i in range(2, numRows):
+            curr = [1]
+            for j in range(i - 1):
+                curr.append(res[i - 1][j] + res[i - 1][j + 1])
+            curr.append(1)
+            res.append(curr)
+        return res
